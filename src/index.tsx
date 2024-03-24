9658b3df015deaef755e45df51cac727bd53bce1
Heedo Kim <devheedoo@gmail.com>
2024-03-25 02:43:49 +0900

    feat: Load popular movies + styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { GlobalStyle } from "./GlobalStyle";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
@@ -12,6 +13,7 @@ const client = new QueryClient();
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
+      <GlobalStyle />
       <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
--- src/pages/popular.tsx
+++ src/pages/popular.tsx
@@ -1,5 +1,78 @@
+import styled from "styled-components";
+import { makeImagePath } from "../api";
+import { useGetPopular } from "../queries";
+
+const PageWrapper = styled.div`
+  margin: 0;
+  padding: 0;
+  background-color: #0e0e0e;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const PageTitle = styled.h1`
+  color: #e0e0e0;
+  font-size: 48px;
+  font-weight: bold;
+  margin: 24px;
+  text-transform: uppercase;
+`;
+
+const MovieList = styled.div`
+  margin-top: 24px;
+  padding: 0 60px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 60px;
+`;
+
+const MovieCard = styled.div`
+  width: 300px;
+  overflow: hidden;
+  border-radius: 24px;
+  background-color: #0f0f0f;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+  padding-bottom: 16px;
+`;
+
+const MovieCardImage = styled.img`
+  width: 100%;
+  height: auto;
+  mask-image: linear-gradient(to bottom, white 70%, transparent);
+`;
+
+const MovieCardTitle = styled.span`
+  color: #e0e0e0;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`;
+
 const Popular = () => {
-  return <h1>Popular</h1>;
+  const { data: popularMovies, isLoading } = useGetPopular();
+  if (isLoading) {
+    return <p>Loading popular movies...</p>;
+  }
+
+  return (
+    <PageWrapper>
+      <PageTitle>Popular</PageTitle>
+      <MovieList>
+        {popularMovies?.results.map((movie) => {
+          return (
+            <MovieCard key={movie.id}>
+              <MovieCardImage src={makeImagePath(movie.poster_path)} />
+              <MovieCardTitle>{movie.title}</MovieCardTitle>
+            </MovieCard>
+          );
+        })}
+      </MovieList>
+    </PageWrapper>
+  );
 };
 
 export default Popular;
