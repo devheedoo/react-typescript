7a84695160920e01a2b04d2deef4898341deec9b
Heedo Kim <devheedoo@gmail.com>
2024-03-25 02:48:39 +0900

    feat: Load coming-soon, now-playing movies

--- src/pages/coming-soon.tsx
+++ src/pages/coming-soon.tsx
@@ -1,5 +1,35 @@
+import { makeImagePath } from "../api";
+import { useGetComingSoon } from "../queries";
+import {
+  PageWrapper,
+  PageTitle,
+  MovieList,
+  MovieCard,
+  MovieCardImage,
+  MovieCardTitle,
+} from "../components/shared";
+
 const ComingSoon = () => {
-  return <h1>ComingSoon</h1>;
+  const { data: comingSoonMovies, isLoading } = useGetComingSoon();
+  if (isLoading) {
+    return <p>Loading coming soon movies...</p>;
+  }
+
+  return (
+    <PageWrapper>
+      <PageTitle>Coming Soon</PageTitle>
+      <MovieList>
+        {comingSoonMovies?.results.map((movie) => {
+          return (
+            <MovieCard key={movie.id}>
+              <MovieCardImage src={makeImagePath(movie.poster_path)} />
+              <MovieCardTitle>{movie.title}</MovieCardTitle>
+            </MovieCard>
+          );
+        })}
+      </MovieList>
+    </PageWrapper>
+  );
 };
 
 export default ComingSoon;
--- src/pages/now-playing.tsx
+++ src/pages/now-playing.tsx
@@ -1,5 +1,35 @@
+import { makeImagePath } from "../api";
+import { useGetNowPlaying } from "../queries";
+import {
+  PageWrapper,
+  PageTitle,
+  MovieList,
+  MovieCard,
+  MovieCardImage,
+  MovieCardTitle,
+} from "../components/shared";
+
 const NowPlaying = () => {
-  return <h1>NowPlaying</h1>;
+  const { data: nowPlayingMovies, isLoading } = useGetNowPlaying();
+  if (isLoading) {
+    return <p>Loading now playing movies...</p>;
+  }
+
+  return (
+    <PageWrapper>
+      <PageTitle>Now Playing</PageTitle>
+      <MovieList>
+        {nowPlayingMovies?.results.map((movie) => {
+          return (
+            <MovieCard key={movie.id}>
+              <MovieCardImage src={makeImagePath(movie.poster_path)} />
+              <MovieCardTitle>{movie.title}</MovieCardTitle>
+            </MovieCard>
+          );
+        })}
+      </MovieList>
+    </PageWrapper>
+  );
 };
 
 export default NowPlaying;
