4bbdf6e290280d919ba1dbd875938be174bf1a8a
Heedo Kim <devheedoo@gmail.com>
2024-03-25 01:57:55 +0900

    feat: Set routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,19 @@
-import App from "./App";
 import { createBrowserRouter } from "react-router-dom";
+import Popular from "./pages/popular";
+import ComingSoon from "./pages/coming-soon";
+import NowPlaying from "./pages/now-playing";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
-  }
+    element: <Popular />,
+  },
+  {
+    path: "/coming-soon",
+    element: <ComingSoon />,
+  },
+  {
+    path: "/now-playing",
+    element: <NowPlaying />,
+  },
 ]);
