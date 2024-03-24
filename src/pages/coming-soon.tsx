9358f388192a107f854f8e19a1880264f0919072
Heedo Kim <devheedoo@gmail.com>
2024-03-25 04:15:35 +0900

    feat: Add modal to coming-soon, now-playing

--- src/pages/coming-soon.tsx
+++ src/pages/coming-soon.tsx
@@ -2,33 +2,54 @@ import { makeImagePath } from "../api";
 import { useGetComingSoon } from "../queries";
 import {
   PageWrapper,
-  PageTitle,
   MovieList,
   MovieCard,
   MovieCardImage,
   MovieCardTitle,
 } from "../components/shared";
+import { useSearchParams } from "react-router-dom";
+import MovieModal from "../components/movie-modal";
 
-const ComingSoon = () => {
+const NowPlaying = () => {
   const { data: comingSoonMovies, isLoading } = useGetComingSoon();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = searchParams.get("id");
+
+  const selectedMovie = id
+    ? comingSoonMovies?.results.find((movie) => movie.id === +id)
+    : undefined;
+
+  const handleClickCard = (movieId: number) => {
+    setSearchParams({ id: movieId + "" });
+  };
+
   if (isLoading) {
     return <p>Loading coming soon movies...</p>;
   }
 
   return (
-    <PageWrapper>
-      <MovieList>
-        {comingSoonMovies?.results.map((movie) => {
-          return (
-            <MovieCard key={movie.id}>
-              <MovieCardImage src={makeImagePath(movie.poster_path)} />
-              <MovieCardTitle>{movie.title}</MovieCardTitle>
-            </MovieCard>
-          );
-        })}
-      </MovieList>
-    </PageWrapper>
+    <>
+      <PageWrapper>
+        <MovieList>
+          {comingSoonMovies?.results.map((movie) => {
+            return (
+              <MovieCard
+                key={movie.id}
+                layoutId={movie.id + ""}
+                onClick={() => handleClickCard(movie.id)}
+              >
+                <MovieCardImage src={makeImagePath(movie.poster_path)} />
+                <MovieCardTitle>{movie.title}</MovieCardTitle>
+              </MovieCard>
+            );
+          })}
+        </MovieList>
+      </PageWrapper>
+
+      <MovieModal movieId={id} selectedMovie={selectedMovie} />
+    </>
   );
 };
 
-export default ComingSoon;
+export default NowPlaying;
--- src/pages/now-playing.tsx
+++ src/pages/now-playing.tsx
@@ -2,32 +2,53 @@ import { makeImagePath } from "../api";
 import { useGetNowPlaying } from "../queries";
 import {
   PageWrapper,
-  PageTitle,
   MovieList,
   MovieCard,
   MovieCardImage,
   MovieCardTitle,
 } from "../components/shared";
+import { useSearchParams } from "react-router-dom";
+import MovieModal from "../components/movie-modal";
 
 const NowPlaying = () => {
   const { data: nowPlayingMovies, isLoading } = useGetNowPlaying();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = searchParams.get("id");
+
+  const selectedMovie = id
+    ? nowPlayingMovies?.results.find((movie) => movie.id === +id)
+    : undefined;
+
+  const handleClickCard = (movieId: number) => {
+    setSearchParams({ id: movieId + "" });
+  };
+
   if (isLoading) {
     return <p>Loading now playing movies...</p>;
   }
 
   return (
-    <PageWrapper>
-      <MovieList>
-        {nowPlayingMovies?.results.map((movie) => {
-          return (
-            <MovieCard key={movie.id}>
-              <MovieCardImage src={makeImagePath(movie.poster_path)} />
-              <MovieCardTitle>{movie.title}</MovieCardTitle>
-            </MovieCard>
-          );
-        })}
-      </MovieList>
-    </PageWrapper>
+    <>
+      <PageWrapper>
+        <MovieList>
+          {nowPlayingMovies?.results.map((movie) => {
+            return (
+              <MovieCard
+                key={movie.id}
+                layoutId={movie.id + ""}
+                onClick={() => handleClickCard(movie.id)}
+              >
+                <MovieCardImage src={makeImagePath(movie.poster_path)} />
+                <MovieCardTitle>{movie.title}</MovieCardTitle>
+              </MovieCard>
+            );
+          })}
+        </MovieList>
+      </PageWrapper>
+
+      <MovieModal movieId={id} selectedMovie={selectedMovie} />
+    </>
   );
 };
 
